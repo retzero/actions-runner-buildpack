7b1f8b09c58241ba1d22ff16a82139526913b246
retzero <ticce@naver.com>
2021-06-02 02:22:20 +0900

    sudo

--- install_actions.sh
+++ install_actions.sh
@@ -12,5 +12,5 @@ fi
 curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
 tar -zxf actions.tar.gz
 rm -f actions.tar.gz
-#./bin/installdependencies.sh
-mkdir /_work
+sudo ./bin/installdependencies.sh
+sudo mkdir /_work && sudo chown actions:actions /_work
